05091194f5efbb81ef093ebb5a2e391085ed6b4a
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-11 23:51:24 +0900

    initial commit

---
+++ src/test/java/com/app/conation/ConationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.conation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
