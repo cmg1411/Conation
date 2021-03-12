9841703e8ddc944e8dc23b12db2aa2f22207530e
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-12 18:41:10 +0900

    feat : 응답 추가

--- src/main/java/com/app/conation/response/BaseResponseStatus.java
+++ src/main/java/com/app/conation/response/BaseResponseStatus.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum BaseResponseStatus {
     SUCCESS(true, 1000, "요청에 성공하였습니다."),
     PLEASE_CHECK_REQUEST(false, 2000, "입력값을 확인해주세요."),
-    NOT_FOUND_ADVERTISEMENT(false, 3000, "존재하지 않는 광고입니다.");
+    NOT_FOUND_ADVERTISEMENT(false, 3000, "존재하지 않는 광고입니다."),
+    NOT_FOUND_REGION(false, 3100, "존재하지 않는 지역입니다."),
+    NOT_FOUND_DONATION_STATUS(false, 3200, "존재하지 않는 지역 기부 현황입니다.");
 
     private boolean isSuccess;
     private int code;
