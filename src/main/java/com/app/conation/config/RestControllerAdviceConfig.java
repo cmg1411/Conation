18e20bc6263711f161d187e53381d7bd19f4c039
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-12 16:59:02 +0900

    feat : 예외 처리 설정 추가

---
+++ src/main/java/com/app/conation/config/RestControllerAdviceConfig.java
@@ -0,0 +1,28 @@
+package com.app.conation.config;
+
+import com.app.conation.exception.BaseException;
+import com.app.conation.response.BaseResponse;
+import com.app.conation.response.BaseResponseStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class RestControllerAdviceConfig {
+
+    @ExceptionHandler(BaseException.class)
+    public BaseResponse baseExceptionHandler(BaseException e) {
+        return new BaseResponse(e.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseResponse<List<String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        List<String> errorMessages = new ArrayList<>();
+        e.getBindingResult().getFieldErrors()
+                .forEach(fieldError -> errorMessages.add(fieldError.getDefaultMessage()));
+        return new BaseResponse(BaseResponseStatus.PLEASE_CHECK_REQUEST, errorMessages);
+    }
+}
