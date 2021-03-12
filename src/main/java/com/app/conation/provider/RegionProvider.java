5aabe923337010abeb8affab56822be508b0cb6f
banjjoknim <zhfxmtkachdt@daum.net>
2021-03-12 18:57:10 +0900

    feat : 지역 조회 메서드 추가

---
+++ src/main/java/com/app/conation/provider/RegionProvider.java
@@ -0,0 +1,31 @@
+package com.app.conation.provider;
+
+import com.app.conation.domain.Region;
+import com.app.conation.domain.RegionRepository;
+import com.app.conation.exception.BaseException;
+import com.app.conation.response.BaseResponseStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional(readOnly = true)
+@Service
+public class RegionProvider {
+
+    private final RegionRepository regionRepository;
+
+    @Autowired
+    public RegionProvider(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    public Region getRegionById(Long regionId) {
+        Optional<Region> region = regionRepository.findById(regionId);
+        if (region.isEmpty()) {
+            throw new BaseException(BaseResponseStatus.NOT_FOUND_REGION);
+        }
+        return region.get();
+    }
+}
